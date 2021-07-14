dca0fe11a4a6ecb902f34e5c02228cdd4faba93e
kylec <KYLECOHEN14@GMAIL.COM>
2021-07-14 19:00:09 -0400

    2nd push

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -40,20 +40,4 @@ class HornedBeast extends React.Component {
       );
     }
 }
-
-// {/* 
-
-//   render() {
-//     return (
-//       <div classname="HornedBeast">
-//         <h2>
-//           {this.props.name}
-//         </h2>
-//         <img src = "https://wallpaperxyz.com/wp-content/uploads/Rhino-HD-Wallpapers-Free-Download-Wallpaperxyz.com-4.jpg" alt = "rhino" title = "rhino"></img>
-//         <p>{this.props.description}</p>
-//       </div>
-//     );
-//   } */}
-
-
 export default HornedBeast;
