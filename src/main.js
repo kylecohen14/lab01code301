035a7eb27130575f288acd3d869a7b06d1fb9791
kylec <KYLECOHEN14@GMAIL.COM>
2021-07-13 21:41:34 -0400

    images and text rendering

--- src/main.js
+++ src/main.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import HornedBeast from './HornedBeast.js';
+import data from './data.json';
 
 class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: data
+    };
+  }
+  
+  beastArr = (data) => {
+    let newArr = data.map( (value) => {
+      return <HornedBeast title={value.title} image_url={value.image_url} description={value.description} horns={value.horns} />;
+    });
+    return newArr;
+  };
+
+
+
+
   render() {
     return (
       <div className="main">
-
-        <div>
-          <HornedBeast name={'rhino'} description={'Big ol rhino'} />
-        </div>
-
-        <div>
-          <HornedBeast name={'rhino'} description={'Even bigger ol rhino'} />
-        </div>
+        {this.beastArr(data)}
       </div>
     );
   }
