5245497bb3ff12e71baddaa4d59fdeebe8f8c3e6
kylec <KYLECOHEN14@GMAIL.COM>
2021-07-16 18:54:16 -0400

    lab 4 complete

--- src/App.js
+++ src/App.js
@@ -5,15 +5,25 @@ import Footer from './footer.js';
 import './style.css';
 import data from './data.json';
 import Selectedbeast from './Selectedbeast.js';
+import BeastForm from './BeastForm.js';
+// import Button from "react-bootstrap/Button";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: data,
+      beastData: data,
       showModal: false,
       selectBeast: {},
+      selectForm: {},
+      selectHorns: ""
     };
+  }
+  
+  submitForm = e => {
+    e.preventDefault();
+    let selectForm = data.find(value => value.horns);
+    this.setState({selectForm});
   }
   showAsModal = (beast) => {
     console.log(beast);
@@ -23,23 +33,38 @@ class App extends React.Component {
     closeModal = () => {
       this.setState({showModal: false});
     }
-
-
-
+    filterHorns = (e) => {
+      e.preventDefault();
+      let horns = data.filter((value) => value.horns === parseInt(this.state.selectHorns));
+      this.setState({beastData: horns});
+    }
+    selectForm = e => {
+      this.setState({selectHorns: e.target.value});
+    }
     render () {
       return (
         <div className="app">
           <Header />
-          <Main data={this.state.data}
+          <BeastForm 
+            selectForm={this.selectForm}
+            formSubmited={this.submitForm}
+            filterHorns={this.filterHorns}/>
+          <Main 
+            // data={horns}
+            data={this.state.beastData}
             showAsModal={this.showAsModal}/>
-          <Selectedbeast selectBeast={this.state.selectBeast}
+          <Selectedbeast 
+            selectBeast={this.state.selectBeast}
             modalOn={this.state.showModal}
             closeModal ={this.closeModal}
           />
           <Footer />
+          {/* <Button onClick={this.selectedHorns}/> */}
         </div>
       );
     }
 }
 
 export default App;
+
+//  dont forget to add form into the render
--- src/main.js
+++ src/main.js
@@ -18,26 +18,4 @@ class Main extends React.Component {
     );
   }
 }
-//<div>
-//   {this.props.allBeasts.map((value, idx) => (
-//     <HornedBeast
-//       key={idx}
-//       index={idx}
-//       src={value.image_url}
-//       title={value.title}
-//       description={value.description}
-//       displayAsModal={this.props.displayAsModal}
-//     />
-//   ))}
-//   </div>
-// }}}
-
-
-
-// beastArr = (data) => {
-//   let newArr = data.map( (value) => {
-//     return <HornedBeast title={value.title} image_url={value.image_url} description={value.description} horns={value.horns} key={value.image_url} />;
-//   });
-//   return newArr;
-// };
 export default Main;
